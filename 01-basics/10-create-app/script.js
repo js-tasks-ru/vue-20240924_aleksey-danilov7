99ff29f31660a48a60c53cf6d905f9c2db45e697
adanilov <adanilov@mmtr.ru>
2024-10-02 17:40:10 +0300

    Задача с выводом текущей даты

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,24 @@
-import { defineComponent, createApp } from 'vue'
+import {defineComponent, createApp} from 'vue'
+
+const App = defineComponent({
+  name: "App",
+
+  setup() {
+    function getDateAtLocalString(timestamp) {
+      return new Date(timestamp).toLocaleString(navigator.language, { dateStyle: 'long'})
+    }
+
+    return {
+      getDateAtLocalString,
+    }
+  },
+
+  template: `<time>Сегодня {{getDateAtLocalString(new Date)}}</time>`
+})
+
+
+const app = createApp(App)
+const vm = app.mount('#app')
+
+window.vm = vm
+
